ec26890dcb42f93f8cfabf0a11f4a30da83c1f92
mrTeroblaze <mramaterus@gmail.com>
2025-08-14 21:14:18 +0300

    test build 0.0.7-test

--- src/main/java/com/mrteroblaze/travelanchorfix/client/ClientProxy.java
+++ src/main/java/com/mrteroblaze/travelanchorfix/client/ClientProxy.java
@@ -1,11 +1,11 @@
 package com.mrteroblaze.travelanchorfix.client;
 
 import com.mrteroblaze.travelanchorfix.client.render.TravelAnchorTESRFix;
+import crazypants.enderio.teleport.anchor.TileTravelAnchor;
 import com.mrteroblaze.travelanchorfix.server.ServerProxy;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import crazypants.enderio.teleport.anchor.TileTravelAnchor;
 
 public class ClientProxy extends ServerProxy {
 
@@ -17,6 +17,7 @@ public class ClientProxy extends ServerProxy {
     @Override
     public void init(FMLInitializationEvent event) {
         super.init(event);
+        // Регистрируем наш фиксированный рендер Travel Anchor
         ClientRegistry.bindTileEntitySpecialRenderer(TileTravelAnchor.class, new TravelAnchorTESRFix());
     }
 }
--- src/main/java/com/mrteroblaze/travelanchorfix/client/render/TravelAnchorTESRFix.java
+++ src/main/java/com/mrteroblaze/travelanchorfix/client/render/TravelAnchorTESRFix.java
@@ -6,16 +6,14 @@ import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.MovingObjectPosition;
-
 import org.lwjgl.opengl.GL11;
-
 import crazypants.enderio.teleport.anchor.TileTravelAnchor;
 import crazypants.enderio.config.Config;
 
 public class TravelAnchorTESRFix extends TileEntitySpecialRenderer {
 
     @Override
-    public void renderTileEntityAt(TileEntity te, double x, double y, double z, float partialTick) {
+    public void renderTileEntityAt(TileEntity te, double x, double y, double z, float partialTicks) {
         if (!(te instanceof TileTravelAnchor)) return;
 
         TileTravelAnchor anchor = (TileTravelAnchor) te;
@@ -24,27 +22,42 @@ public class TravelAnchorTESRFix extends TileEntitySpecialRenderer {
 
         Minecraft mc = Minecraft.getMinecraft();
         double distSq = mc.thePlayer.getDistanceSq(te.xCoord + 0.5, te.yCoord + 0.5, te.zCoord + 0.5);
-        if (distSq > Config.travelAnchorMaxDistanceSq) return;
 
-        // Проверяем, что наведено именно на этот якорь
+        // Проверка на дистанцию (в квадрате)
+        if (distSq > Config.travelAnchorMaxDistance * Config.travelAnchorMaxDistance) {
+            return;
+        }
+
+        // Проверка — только если мы наводимся на этот блок
         MovingObjectPosition mop = mc.objectMouseOver;
         if (mop == null || mop.blockX != te.xCoord || mop.blockY != te.yCoord || mop.blockZ != te.zCoord) {
             return;
         }
 
         GL11.glPushMatrix();
-        GL11.glTranslated(x + 0.5, y + 1.3, z + 0.5);
+        GL11.glTranslated(x + 0.5, y + 1.5, z + 0.5);
         GL11.glNormal3f(0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(-mc.getRenderManager().playerViewY, 0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(mc.getRenderManager().playerViewX, 1.0F, 0.0F, 0.0F);
+        GL11.glRotatef(-mc.renderViewEntity.rotationYaw, 0.0F, 1.0F, 0.0F);
+        GL11.glRotatef(mc.renderViewEntity.rotationPitch, 1.0F, 0.0F, 0.0F);
         GL11.glScalef(-0.025F, -0.025F, 0.025F);
 
+        FontRenderer fontrenderer = this.func_147498_b(); // getFontRenderer() для 1.7.10
+        int strWidth = fontrenderer.getStringWidth(name) / 2;
+
+        // Рамка (фон)
         GL11.glDisable(GL11.GL_LIGHTING);
         GL11.glDisable(GL11.GL_DEPTH_TEST);
+        GL11.glTranslatef(0.0F, 0.0F, 0.01F);
+        GL11.glBegin(GL11.GL_QUADS);
+        GL11.glColor4f(0.0F, 0.0F, 0.0F, 0.5F);
+        GL11.glVertex3f(-strWidth - 2, -2, 0.0F);
+        GL11.glVertex3f(-strWidth - 2, 8, 0.0F);
+        GL11.glVertex3f(strWidth + 2, 8, 0.0F);
+        GL11.glVertex3f(strWidth + 2, -2, 0.0F);
+        GL11.glEnd();
 
-        FontRenderer fontrenderer = Minecraft.getMinecraft().fontRenderer;
-        int width = fontrenderer.getStringWidth(name) / 2;
-        fontrenderer.drawString(EnumChatFormatting.AQUA + name, -width, 0, 0xFFFFFF);
+        // Сам текст
+        fontrenderer.drawString(EnumChatFormatting.WHITE + name, -strWidth, 0, 0xFFFFFF);
 
         GL11.glEnable(GL11.GL_DEPTH_TEST);
         GL11.glEnable(GL11.GL_LIGHTING);
