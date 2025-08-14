ec2ef3646b6b90e6c98c76b9d443da69521f89d9
mrTeroblaze <mramaterus@gmail.com>
2025-08-14 21:06:21 +0300

    test build 0.0.4-test

--- src/main/java/com/mrteroblaze/travelanchorfix/client/render/TravelAnchorTESRFix.java
+++ src/main/java/com/mrteroblaze/travelanchorfix/client/render/TravelAnchorTESRFix.java
@@ -1,93 +1,78 @@
 package com.mrteroblaze.travelanchorfix.client.render;
 
-import cpw.mods.fml.client.registry.ClientRegistry;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.client.gui.FontRenderer;
+
 import org.lwjgl.opengl.GL11;
+
 import crazypants.enderio.teleport.anchor.TileTravelAnchor;
 import crazypants.enderio.config.Config;
 
 public class TravelAnchorTESRFix extends TileEntitySpecialRenderer {
 
     @Override
-    public void renderTileEntityAt(TileEntity te, double x, double y, double z, float partialTick) {
-        if (!(te instanceof TileTravelAnchor)) return;
+    public void renderTileEntityAt(TileEntity te, double x, double y, double z, float partialTicks) {
+        if (!(te instanceof TileTravelAnchor)) {
+            return;
+        }
 
         TileTravelAnchor anchor = (TileTravelAnchor) te;
         String name = anchor.getLabel();
         if (name == null || name.trim().isEmpty()) return;
 
         Minecraft mc = Minecraft.getMinecraft();
-        EntityPlayer player = mc.thePlayer;
-        double distSq = player.getDistanceSq(te.xCoord + 0.5, te.yCoord + 0.5, te.zCoord + 0.5);
+        double distSq = mc.thePlayer.getDistanceSq(te.xCoord + 0.5, te.yCoord + 0.5, te.zCoord + 0.5);
 
-        // Проверка расстояния
-        if (distSq > Config.travelAnchorMaxDistanceSq) return;
+        double maxDistSq = Config.travelAnchorMaxDistance * Config.travelAnchorMaxDistance;
+        if (distSq > maxDistSq) return;
 
-        // Проверка на возможность телепортации (условия EnderIO)
-        if (!anchor.canSee(player)) return;
+        // Отображаем только если игрок целится
+        if (!mc.objectMouseOver.getBlockPos().equals(te.getPos())) {
+            return;
+        }
 
-        // Рендер текста
         GL11.glPushMatrix();
-        GL11.glTranslated(x + 0.5, y + 1.2, z + 0.5);
+        GL11.glTranslated(x + 0.5, y + 1.5, z + 0.5);
         GL11.glNormal3f(0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(-mc.thePlayer.rotationYaw, 0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(mc.thePlayer.rotationPitch, 1.0F, 0.0F, 0.0F);
-        float scale = 0.016F * 1.2F;
+
+        // Разворачиваем к игроку
+        RenderManager renderManager = RenderManager.instance;
+        GL11.glRotatef(-renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
+        GL11.glRotatef(renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
+
+        float scale = 0.016F * 1.6F;
         GL11.glScalef(-scale, -scale, scale);
-        mc.fontRenderer.drawString(EnumChatFormatting.AQUA + name, -mc.fontRenderer.getStringWidth(name) / 2, 0, 0xFFFFFF);
-        GL11.glPopMatrix();
-
-        // Рисуем рамку
-        drawSelectionBox(x, y, z);
-    }
-
-    private void drawSelectionBox(double x, double y, double z) {
-        GL11.glPushMatrix();
-        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glDepthMask(false);
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-        GL11.glLineWidth(2.0F);
-        GL11.glColor4f(0.0F, 1.0F, 1.0F, 0.5F);
 
-        Tessellator tes = Tessellator.instance;
-        tes.startDrawing(GL11.GL_LINES);
-
-        // Каркас блока (1x1x1)
-        tes.addVertex(x, y, z);
-        tes.addVertex(x + 1, y, z);
-        tes.addVertex(x + 1, y, z);
-        tes.addVertex(x + 1, y, z + 1);
-        tes.addVertex(x + 1, y, z + 1);
-        tes.addVertex(x, y, z + 1);
-        tes.addVertex(x, y, z + 1);
-        tes.addVertex(x, y, z);
-
-        tes.addVertex(x, y + 1, z);
-        tes.addVertex(x + 1, y + 1, z);
-        tes.addVertex(x + 1, y + 1, z);
-        tes.addVertex(x + 1, y + 1, z + 1);
-        tes.addVertex(x + 1, y + 1, z + 1);
-        tes.addVertex(x, y + 1, z + 1);
-        tes.addVertex(x, y + 1, z + 1);
-        tes.addVertex(x, y + 1, z);
-
-        tes.addVertex(x, y, z);
-        tes.addVertex(x, y + 1, z);
-        tes.addVertex(x + 1, y, z);
-        tes.addVertex(x + 1, y + 1, z);
-        tes.addVertex(x + 1, y, z + 1);
-        tes.addVertex(x + 1, y + 1, z + 1);
-        tes.addVertex(x, y, z + 1);
-        tes.addVertex(x, y + 1, z + 1);
-
-        tes.draw();
-
+        // Отрисовка фона
+        FontRenderer fontrenderer = this.getFontRenderer();
+        int stringWidth = fontrenderer.getStringWidth(name) / 2;
+        Tessellator tessellator = Tessellator.instance;
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        tessellator.startDrawingQuads();
+        tessellator.setColorRGBA_F(0.0F, 0.0F, 0.0F, 0.25F);
+        tessellator.addVertex(-stringWidth - 2, -1, 0.0D);
+        tessellator.addVertex(-stringWidth - 2, 8, 0.0D);
+        tessellator.addVertex(stringWidth + 2, 8, 0.0D);
+        tessellator.addVertex(stringWidth + 2, -1, 0.0D);
+        tessellator.draw();
         GL11.glEnable(GL11.GL_TEXTURE_2D);
+
+        // Отрисовка текста
+        fontrenderer.drawString(EnumChatFormatting.WHITE + name, -stringWidth, 0, 0xFFFFFF);
+
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glDepthMask(true);
+        GL11.glEnable(GL11.GL_LIGHTING);
         GL11.glDisable(GL11.GL_BLEND);
         GL11.glPopMatrix();
     }
