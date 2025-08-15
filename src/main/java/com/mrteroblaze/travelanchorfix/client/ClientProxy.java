9ace431539fce809f37b8b0b473929bd62f63c66
mrTeroblaze <mramaterus@gmail.com>
2025-08-15 16:12:51 +0300

    test build 0.0.29-test

--- src/main/java/com/mrteroblaze/travelanchorfix/client/ClientProxy.java
+++ src/main/java/com/mrteroblaze/travelanchorfix/client/ClientProxy.java
@@ -1,14 +1,35 @@
 package com.mrteroblaze.travelanchorfix.client;
 
-import com.mrteroblaze.travelanchorfix.handler.ClientTickHandlerFix;
-import cpw.mods.fml.common.FMLCommonHandler;
+import com.mrteroblaze.travelanchorfix.client.render.TravelAnchorTESRFix;
+import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import crazypants.enderio.teleport.anchor.TileTravelAnchor;
 
 public class ClientProxy extends CommonProxy {
 
     @Override
-    public void init() {
-        super.init();
-        // Регистрируем обработчик тиков
-        FMLCommonHandler.instance().bus().register(new ClientTickHandlerFix());
+    public void preInit(FMLPreInitializationEvent event) {
+        super.preInit(event);
+    }
+
+    @Override
+    public void init(FMLInitializationEvent event) {
+        super.init(event);
+        // Регистрируем наш TESR для Travel Anchor
+        TravelAnchorTESRFix.register();
+    }
+
+    @Override
+    public World getClientWorld() {
+        return FMLClientHandler.instance().getClient().theWorld;
+    }
+
+    @Override
+    public EntityPlayer getClientPlayer() {
+        return Minecraft.getMinecraft().thePlayer;
     }
 }
