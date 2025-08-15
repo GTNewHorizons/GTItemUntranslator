f86eacad367b6aae81f3400db73175abd7874e90
mrTeroblaze <mramaterus@gmail.com>
2025-08-15 23:33:15 +0300

    test build 0.0.61-test

--- src/main/java/com/mrteroblaze/travelanchorfix/client/render/MyTravelEntitySpecialRenderer.java
+++ src/main/java/com/mrteroblaze/travelanchorfix/client/render/MyTravelEntitySpecialRenderer.java
@@ -4,27 +4,35 @@ import crazypants.enderio.teleport.anchor.TileTravelAnchor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.tileentity.TileEntity;
 import org.lwjgl.opengl.GL11;
 
-public class MyTravelEntitySpecialRenderer extends TileEntitySpecialRenderer<TileTravelAnchor> {
+public class MyTravelEntitySpecialRenderer extends TileEntitySpecialRenderer {
 
     @Override
-    public void renderTileEntityAt(TileTravelAnchor te, double x, double y, double z, float partialTick) {
-        String name = te.getLabel();
+    public void renderTileEntityAt(TileEntity te, double x, double y, double z, float partialTick) {
+        if (!(te instanceof TileTravelAnchor)) {
+            return;
+        }
+
+        TileTravelAnchor anchor = (TileTravelAnchor) te;
+        String name = anchor.getLabel();
         if (name == null || name.trim().isEmpty()) {
             return;
         }
 
         FontRenderer fr = Minecraft.getMinecraft().fontRenderer;
         boolean oldUnicode = fr.getUnicodeFlag();
-        fr.setUnicodeFlag(true); // включаем Unicode для устранения квадратиков
+        fr.setUnicodeFlag(true); // включаем Unicode, чтобы убрать квадратики
 
         GL11.glPushMatrix();
         GL11.glTranslated(x + 0.5, y + 1.2, z + 0.5);
 
-        // Поворот к игроку (MC 1.7.10)
-        GL11.glRotatef(-this.func_147498_b().playerViewY, 0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(this.func_147498_b().playerViewX, 1.0F, 0.0F, 0.0F);
+        // Поворот к игроку
+        TileEntityRendererDispatcher renderer = TileEntityRendererDispatcher.instance;
+        GL11.glRotatef(-renderer.playerViewY, 0.0F, 1.0F, 0.0F);
+        GL11.glRotatef(renderer.playerViewX, 1.0F, 0.0F, 0.0F);
 
         GL11.glScalef(-0.025F, -0.025F, 0.025F);
         GL11.glDisable(GL11.GL_LIGHTING);
@@ -37,6 +45,6 @@ public class MyTravelEntitySpecialRenderer extends TileEntitySpecialRenderer<Til
         GL11.glEnable(GL11.GL_LIGHTING);
         GL11.glPopMatrix();
 
-        fr.setUnicodeFlag(oldUnicode); // возвращаем флаг
+        fr.setUnicodeFlag(oldUnicode); // возвращаем старый флаг
     }
 }
