15a9b7240fceada4f67a585cbeb137209226c259
mrTeroblaze <mramaterus@gmail.com>
2025-08-16 02:37:36 +0300

    test build 0.0.69-test

--- src/main/java/com/mrteroblaze/travelanchorfix/TravelAnchorFix.java
+++ src/main/java/com/mrteroblaze/travelanchorfix/TravelAnchorFix.java
@@ -14,6 +14,7 @@ public class TravelAnchorFix {
 
     public static final String MODID = "travelanchorfix";
     public static final String MODNAME = "Travel Anchor Fix";
+	public static final String VERSION = "1.0.0";
 
     @SidedProxy(
         clientSide = "com.mrteroblaze.travelanchorfix.client.ClientProxy",
