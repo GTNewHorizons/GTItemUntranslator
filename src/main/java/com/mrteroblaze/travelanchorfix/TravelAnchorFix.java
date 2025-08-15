d43f756f0cc606ad89b546c40200e62bf7ea48ca
mrTeroblaze <mramaterus@gmail.com>
2025-08-15 21:50:19 +0300

    test build 0.0.52-test

--- src/main/java/com/mrteroblaze/travelanchorfix/TravelAnchorFix.java
+++ src/main/java/com/mrteroblaze/travelanchorfix/TravelAnchorFix.java
@@ -1,29 +1,23 @@
 package com.mrteroblaze.travelanchorfix;
 
 import com.mrteroblaze.travelanchorfix.client.ClientProxy;
-
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 
-@Mod(
-    modid = TravelAnchorFix.MODID,
-    name = TravelAnchorFix.MODNAME,
-    version = TravelAnchorFix.VERSION,
-    dependencies = "required-after:EnderIO")
+@Mod(modid = TravelAnchorFix.MODID, name = TravelAnchorFix.MODNAME, version = TravelAnchorFix.VERSION, dependencies = "required-after:EnderIO")
 public class TravelAnchorFix {
 
     public static final String MODID = "travelanchorfix";
     public static final String MODNAME = "Travel Anchor Fix";
     public static final String VERSION = "1.0.0";
 
-    @SidedProxy(
-        clientSide = "com.mrteroblaze.travelanchorfix.client.ClientProxy",
-        serverSide = "com.mrteroblaze.travelanchorfix.client.ClientProxy") // на сервере просто пустой init
+    @SidedProxy(clientSide = "com.mrteroblaze.travelanchorfix.client.ClientProxy",
+                serverSide = "com.mrteroblaze.travelanchorfix.client.ClientProxy")
     public static ClientProxy proxy;
 
     @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
+    public void postInit(FMLPostInitializationEvent event) {
         proxy.init();
     }
 }
