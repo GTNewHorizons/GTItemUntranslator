e8593441615a733c4c5e180830d3d8e658a70244
mrTeroblaze <mramaterus@gmail.com>
2025-08-15 23:28:10 +0300

    test build 0.0.60-test

--- src/main/java/com/mrteroblaze/travelanchorfix/client/render/MyTravelEntitySpecialRenderer.java
+++ src/main/java/com/mrteroblaze/travelanchorfix/client/render/MyTravelEntitySpecialRenderer.java
@@ -4,31 +4,25 @@ import crazypants.enderio.teleport.anchor.TileTravelAnchor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.tileentity.TileEntity;
 import org.lwjgl.opengl.GL11;
 
-public class MyTravelEntitySpecialRenderer extends TileEntitySpecialRenderer {
+public class MyTravelEntitySpecialRenderer extends TileEntitySpecialRenderer<TileTravelAnchor> {
 
     @Override
-    public void renderTileEntityAt(TileEntity te, double x, double y, double z, float partialTick) {
-        if (!(te instanceof TileTravelAnchor)) {
-            return;
-        }
-
-        TileTravelAnchor anchor = (TileTravelAnchor) te;
-        String name = anchor.getLabel();
+    public void renderTileEntityAt(TileTravelAnchor te, double x, double y, double z, float partialTick) {
+        String name = te.getLabel();
         if (name == null || name.trim().isEmpty()) {
             return;
         }
 
         FontRenderer fr = Minecraft.getMinecraft().fontRenderer;
         boolean oldUnicode = fr.getUnicodeFlag();
-        fr.setUnicodeFlag(true); // включаем Unicode, чтобы не было квадратиков
+        fr.setUnicodeFlag(true); // включаем Unicode для устранения квадратиков
 
         GL11.glPushMatrix();
         GL11.glTranslated(x + 0.5, y + 1.2, z + 0.5);
 
-        // Поворот к игроку (метод func_147498_b() есть в 1.7.10)
+        // Поворот к игроку (MC 1.7.10)
         GL11.glRotatef(-this.func_147498_b().playerViewY, 0.0F, 1.0F, 0.0F);
         GL11.glRotatef(this.func_147498_b().playerViewX, 1.0F, 0.0F, 0.0F);
 
@@ -43,6 +37,6 @@ public class MyTravelEntitySpecialRenderer extends TileEntitySpecialRenderer {
         GL11.glEnable(GL11.GL_LIGHTING);
         GL11.glPopMatrix();
 
-        fr.setUnicodeFlag(oldUnicode); // возвращаем старое состояние
+        fr.setUnicodeFlag(oldUnicode); // возвращаем флаг
     }
 }
