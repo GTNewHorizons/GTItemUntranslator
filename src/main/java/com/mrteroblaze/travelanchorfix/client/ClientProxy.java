78fe9f2d2e819a5ca8183ff1c588a72bc4b9acf5
mrTeroblaze <mramaterus@gmail.com>
2025-08-15 17:10:52 +0300

    test build 0.0.33-test

--- src/main/java/com/mrteroblaze/travelanchorfix/client/ClientProxy.java
+++ src/main/java/com/mrteroblaze/travelanchorfix/client/ClientProxy.java
@@ -2,20 +2,21 @@ package com.mrteroblaze.travelanchorfix.client;
 
 import com.mrteroblaze.travelanchorfix.client.handler.ClientTickHandlerFix;
 import com.mrteroblaze.travelanchorfix.client.render.TravelAnchorTESRFix;
+import com.mrteroblaze.travelanchorfix.common.CommonProxy;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.common.FMLCommonHandler;
 import crazypants.enderio.teleport.anchor.TileTravelAnchor;
+import net.minecraftforge.common.MinecraftForge;
 
 public class ClientProxy extends CommonProxy {
 
-    @Override
     public void init() {
-        super.init();
-
-        // Регистрируем фиксированный TESR для Travel Anchor
+        // Bind our TESR to Travel Anchor
         ClientRegistry.bindTileEntitySpecialRenderer(TileTravelAnchor.class, new TravelAnchorTESRFix());
 
-        // Регистрируем наш фикс тик-хендлера
-        FMLCommonHandler.instance().bus().register(new ClientTickHandlerFix());
+        // Register tick handler (both FML bus and Forge bus for safety in GTNH)
+        ClientTickHandlerFix tickHandler = new ClientTickHandlerFix();
+        FMLCommonHandler.instance().bus().register(tickHandler);
+        MinecraftForge.EVENT_BUS.register(tickHandler);
     }
 }
