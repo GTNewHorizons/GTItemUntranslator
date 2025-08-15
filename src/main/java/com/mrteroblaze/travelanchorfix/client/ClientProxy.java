e412a78317c895f3f78097180931196c05729a53
mrTeroblaze <mramaterus@gmail.com>
2025-08-15 22:41:27 +0300

    test build 0.0.54-test

--- src/main/java/com/mrteroblaze/travelanchorfix/client/ClientProxy.java
+++ src/main/java/com/mrteroblaze/travelanchorfix/client/ClientProxy.java
@@ -1,12 +1,10 @@
 package com.mrteroblaze.travelanchorfix.client;
 
 import com.mrteroblaze.travelanchorfix.client.render.MyTravelEntitySpecialRenderer;
-
 import cpw.mods.fml.client.registry.ClientRegistry;
 import crazypants.enderio.teleport.anchor.TileTravelAnchor;
 
 public class ClientProxy {
-
     public void init() {
         ClientRegistry.bindTileEntitySpecialRenderer(TileTravelAnchor.class, new MyTravelEntitySpecialRenderer());
     }
--- src/main/java/com/mrteroblaze/travelanchorfix/client/render/MyTravelEntitySpecialRenderer.java
+++ src/main/java/com/mrteroblaze/travelanchorfix/client/render/MyTravelEntitySpecialRenderer.java
@@ -1,37 +1,51 @@
 package com.mrteroblaze.travelanchorfix.client.render;
 
+import crazypants.enderio.teleport.anchor.TileTravelAnchor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
+import org.lwjgl.opengl.GL11;
+import java.util.Locale;
 
-import org.lwjgl.util.vector.Vector3f;
-
-import crazypants.enderio.teleport.anchor.TileTravelAnchor;
-import crazypants.enderio.teleport.anchor.TravelEntitySpecialRenderer;
-
-public class MyTravelEntitySpecialRenderer extends TravelEntitySpecialRenderer {
+public class MyTravelEntitySpecialRenderer extends TileEntitySpecialRenderer {
 
     @Override
     public void renderTileEntityAt(TileEntity te, double x, double y, double z, float partialTick) {
-        if (te instanceof TileTravelAnchor) {
-            TileTravelAnchor anchor = (TileTravelAnchor) te;
-
-            FontRenderer fr = Minecraft.getMinecraft().fontRenderer;
-            boolean prevUnicode = fr.getUnicodeFlag();
-            fr.setUnicodeFlag(true);
-
-            super.renderTileEntityAt(te, x, y, z, partialTick);
-
-            fr.setUnicodeFlag(prevUnicode);
-        } else {
-            super.renderTileEntityAt(te, x, y, z, partialTick);
+        if (!(te instanceof TileTravelAnchor)) {
+            return;
         }
-    }
 
-    private Vector3f getAnchorPosition(TileEntity tileentity) {
-        return new Vector3f(
-            (float) tileentity.xCoord + 0.5f,
-            (float) tileentity.yCoord + 0.5f,
-            (float) tileentity.zCoord + 0.5f);
+        TileTravelAnchor anchor = (TileTravelAnchor) te;
+        String name = anchor.getLabel();
+        if (name == null || name.trim().isEmpty()) {
+            return;
+        }
+
+        // --- Включаем Unicode рендер ---
+        FontRenderer fr = Minecraft.getMinecraft().fontRenderer;
+        boolean oldUnicode = fr.getUnicodeFlag();
+        fr.setUnicodeFlag(true);
+
+        GL11.glPushMatrix();
+        GL11.glTranslated(x + 0.5, y + 1.2, z + 0.5);
+
+        // Разворачиваем к игроку
+        GL11.glRotatef(-Minecraft.getMinecraft().getRenderManager().playerViewY, 0.0F, 1.0F, 0.0F);
+        GL11.glRotatef(Minecraft.getMinecraft().getRenderManager().playerViewX, 1.0F, 0.0F, 0.0F);
+
+        GL11.glScalef(-0.025F, -0.025F, 0.025F);
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
+
+        int textWidth = fr.getStringWidth(name);
+        fr.drawString(name, -textWidth / 2, 0, 0xFFFFFF);
+
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glEnable(GL11.GL_LIGHTING);
+        GL11.glPopMatrix();
+
+        // --- Возвращаем старую настройку ---
+        fr.setUnicodeFlag(oldUnicode);
     }
 }
