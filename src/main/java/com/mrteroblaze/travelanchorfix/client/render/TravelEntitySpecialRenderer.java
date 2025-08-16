215e4e3ee97abf61f7cd8e1f7ee5cdb787e7a9cf
mrTeroblaze <mramaterus@gmail.com>
2025-08-16 10:21:33 +0300

    test build 0.0.148-test

--- src/main/java/com/mrteroblaze/travelanchorfix/client/render/TravelEntitySpecialRenderer.java
+++ src/main/java/com/mrteroblaze/travelanchorfix/client/render/TravelEntitySpecialRenderer.java
@@ -3,9 +3,7 @@ package com.mrteroblaze.travelanchorfix.client.render;
 import com.enderio.core.client.render.RenderUtil;
 import com.enderio.core.common.vecmath.Vector3d;
 import crazypants.enderio.EnderIO;
-import crazypants.enderio.machine.travel.BlockTravelAnchor;
-import crazypants.enderio.machine.travel.TileTravelAnchor;
-import cpw.mods.fml.client.registry.ClientRegistry;
+import crazypants.enderio.teleport.TileTravelAnchor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.entity.RenderManager;
@@ -16,8 +14,10 @@ import org.lwjgl.opengl.GL11;
 
 public class TravelEntitySpecialRenderer extends TileEntitySpecialRenderer {
 
-    public static void register() {
-        ClientRegistry.bindTileEntitySpecialRenderer(TileTravelAnchor.class, new TravelEntitySpecialRenderer());
+    private final FontRenderer fr;
+
+    public TravelEntitySpecialRenderer() {
+        this.fr = Minecraft.getMinecraft().fontRenderer;
     }
 
     @Override
@@ -27,7 +27,6 @@ public class TravelEntitySpecialRenderer extends TileEntitySpecialRenderer {
         }
 
         TileTravelAnchor anchor = (TileTravelAnchor) te;
-
         String toRender = anchor.getLabel();
         if (toRender == null || toRender.isEmpty()) {
             return;
@@ -35,16 +34,13 @@ public class TravelEntitySpecialRenderer extends TileEntitySpecialRenderer {
 
         Vector3d pos = new Vector3d(x + 0.5, y + 1.5, z + 0.5);
 
-        final Minecraft mc = Minecraft.getMinecraft();
-        final FontRenderer fr = mc.fontRenderer;
-
         GL11.glPushMatrix();
         try {
             GL11.glTranslatef((float) pos.x, (float) pos.y, (float) pos.z);
 
             RenderManager rm = RenderManager.instance;
             GL11.glRotatef(-rm.playerViewY, 0.0F, 1.0F, 0.0F);
-            GL11.glRotatef(rm.playerViewX, 1.0F, 0.0F, 0.0F);
+            GL11.glRotatef(rm.playerViewX, 1.0F, 0.0F);
 
             final float s = 0.025F * 2.0F;
             GL11.glScalef(-s, -s, s);
@@ -58,15 +54,9 @@ public class TravelEntitySpecialRenderer extends TileEntitySpecialRenderer {
             int textW = fr.getStringWidth(toRender);
             int baseX = -textW / 2;
 
-            // фон-подложка (как в оригинале EnderIO)
+            // фон под текстом
             GL11.glDisable(GL11.GL_TEXTURE_2D);
-            GL11.glBegin(GL11.GL_QUADS);
-            GL11.glColor4f(0.0F, 0.0F, 0.0F, 0.25F);
-            GL11.glVertex3f(baseX - 2, -1, 0.0F);
-            GL11.glVertex3f(baseX - 2, 8, 0.0F);
-            GL11.glVertex3f(baseX + textW + 2, 8, 0.0F);
-            GL11.glVertex3f(baseX + textW + 2, -1, 0.0F);
-            GL11.glEnd();
+            RenderUtil.drawQuad2D(baseX - 1, -1, textW + 2, 8, 0x80000000);
             GL11.glEnable(GL11.GL_TEXTURE_2D);
 
             // сам текст
@@ -81,12 +71,9 @@ public class TravelEntitySpecialRenderer extends TileEntitySpecialRenderer {
         }
     }
 
-    // ==========================
-    // Достаём иконки через рефлексию
-    // ==========================
     private static IIcon getSelectedOverlayIcon() {
         try {
-            java.lang.reflect.Field f = BlockTravelAnchor.class.getDeclaredField("selectedOverlayIcon");
+            java.lang.reflect.Field f = EnderIO.blockTravelPlatform.getClass().getDeclaredField("selectedOverlayIcon");
             f.setAccessible(true);
             return (IIcon) f.get(EnderIO.blockTravelPlatform);
         } catch (Throwable t) {
@@ -97,7 +84,7 @@ public class TravelEntitySpecialRenderer extends TileEntitySpecialRenderer {
 
     private static IIcon getHighlightOverlayIcon() {
         try {
-            java.lang.reflect.Field f = BlockTravelAnchor.class.getDeclaredField("highlightOverlayIcon");
+            java.lang.reflect.Field f = EnderIO.blockTravelPlatform.getClass().getDeclaredField("highlightOverlayIcon");
             f.setAccessible(true);
             return (IIcon) f.get(EnderIO.blockTravelPlatform);
         } catch (Throwable t) {
