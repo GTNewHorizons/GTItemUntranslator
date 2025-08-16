6a9c8d87f5684cdbddcf5c0e0f2c813e5b6b818a
mrTeroblaze <mramaterus@gmail.com>
2025-08-16 05:08:09 +0300

    test build 0.0.94-test

--- src/main/java/com/mrteroblaze/travelanchorfix/client/render/TravelEntitySpecialRenderer.java
+++ src/main/java/com/mrteroblaze/travelanchorfix/client/render/TravelEntitySpecialRenderer.java
@@ -1,4 +1,4 @@
-package crazypants.enderio.teleport.anchor;
+package com.mrteroblaze.travelanchorfix.client.render;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
