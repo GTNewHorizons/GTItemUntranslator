af4395a678385afd5b247d9907b27deaad769ee2
mrTeroblaze <mramaterus@gmail.com>
2025-08-16 09:11:10 +0300

    test build 0.0.142-test

--- src/main/java/com/mrteroblaze/travelanchorfix/client/render/AnchorLabelRenderer.java
+++ src/main/java/com/mrteroblaze/travelanchorfix/client/render/AnchorLabelRenderer.java
@@ -1,69 +1,75 @@
 package com.mrteroblaze.travelanchorfix.client.render;
 
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.tileentity.TileEntity;
-
+import net.minecraft.util.Vec3;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
 import org.lwjgl.opengl.GL11;
 
-public class AnchorLabelRenderer extends TileEntitySpecialRenderer {
+public class AnchorLabelRenderer {
 
     private final Minecraft mc = Minecraft.getMinecraft();
-    private final FontRenderer fontRenderer = mc.fontRenderer;
 
-    @Override
-    public void renderTileEntityAt(TileEntity te, double x, double y, double z, float partialTicks) {
-        if (te == null || te.isInvalid()) {
+    @SubscribeEvent
+    public void onRenderWorldLast(RenderWorldLastEvent event) {
+        if (mc.theWorld == null || mc.thePlayer == null) {
             return;
         }
 
-        // Тестовый текст, потом сюда можно будет подставить имя якоря
-        String text = "Travel Anchor";
+        double px = mc.thePlayer.lastTickPosX + (mc.thePlayer.posX - mc.thePlayer.lastTickPosX) * event.partialTicks;
+        double py = mc.thePlayer.lastTickPosY + (mc.thePlayer.posY - mc.thePlayer.lastTickPosY) * event.partialTicks;
+        double pz = mc.thePlayer.lastTickPosZ + (mc.thePlayer.posZ - mc.thePlayer.lastTickPosZ) * event.partialTicks;
+
+        // Для теста просто выводим всегда рядом с игроком
+        String text = "TravelAnchorFix TEST";
+        double x = px + 2;
+        double y = py + 1.5;
+        double z = pz;
+
+        System.out.println("[TravelAnchorFix] Preparing to draw label: " + text);
+
+        renderLabel(text, x - px, y - py, z - pz);
+    }
+
+    private void renderLabel(String text, double x, double y, double z) {
+        FontRenderer fr = mc.fontRenderer;
 
         GL11.glPushMatrix();
-        GL11.glTranslated(x + 0.5, y + 1.5, z + 0.5);
-
-        // Billboard эффект (разворачиваем текст к игроку)
+        GL11.glTranslated(x, y, z);
         GL11.glNormal3f(0.0F, 1.0F, 0.0F);
         GL11.glRotatef(-RenderManager.instance.playerViewY, 0.0F, 1.0F, 0.0F);
         GL11.glRotatef(RenderManager.instance.playerViewX, 1.0F, 0.0F, 0.0F);
-
-        // Масштаб текста
-        GL11.glScalef(-0.016F * 1.6F, -0.016F * 1.6F, 0.016F * 1.6F);
-
-        // Фон (полупрозрачный прямоугольник)
+        GL11.glScalef(-0.016666668F, -0.016666668F, 0.016666668F);
         GL11.glDisable(GL11.GL_LIGHTING);
         GL11.glDepthMask(false);
         GL11.glDisable(GL11.GL_DEPTH_TEST);
         GL11.glEnable(GL11.GL_BLEND);
-        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
-        int width = fontRenderer.getStringWidth(text);
-        int half = width / 2;
+        int width = fr.getStringWidth(text) / 2;
 
-        Tessellator tess = Tessellator.instance;
+        // фон
         GL11.glDisable(GL11.GL_TEXTURE_2D);
-        tess.startDrawingQuads();
-        tess.setColorRGBA_F(0.0F, 0.0F, 0.0F, 0.5F);
-        tess.addVertex(-half - 2, -2, 0.0D);
-        tess.addVertex(-half - 2, 9, 0.0D);
-        tess.addVertex(half + 2, 9, 0.0D);
-        tess.addVertex(half + 2, -2, 0.0D);
-        tess.draw();
+        GL11.glBegin(GL11.GL_QUADS);
+        GL11.glColor4f(0.0F, 0.0F, 0.0F, 0.25F);
+        GL11.glVertex3f(-width - 2, -2, 0.0F);
+        GL11.glVertex3f(-width - 2, 9, 0.0F);
+        GL11.glVertex3f(width + 2, 9, 0.0F);
+        GL11.glVertex3f(width + 2, -2, 0.0F);
+        GL11.glEnd();
         GL11.glEnable(GL11.GL_TEXTURE_2D);
 
-        // Текст
-        fontRenderer.drawString(text, -half, 0, 0xFFFFFFFF);
+        // сам текст
+        System.out.println("[TravelAnchorFix] Actually drawing label: " + text);
+        fr.drawString(text, -width, 0, 0xFFFFFFFF);
 
-        // Восстановление GL-состояний
         GL11.glEnable(GL11.GL_DEPTH_TEST);
         GL11.glDepthMask(true);
         GL11.glEnable(GL11.GL_LIGHTING);
         GL11.glDisable(GL11.GL_BLEND);
-
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         GL11.glPopMatrix();
     }
 }
