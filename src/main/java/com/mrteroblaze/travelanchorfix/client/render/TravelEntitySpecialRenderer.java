4f39e97e88973659dde2bc43bb44b90873371a3c
mrTeroblaze <mramaterus@gmail.com>
2025-08-16 12:00:58 +0300

    test build 0.0.161-test

--- src/main/java/com/mrteroblaze/travelanchorfix/client/render/TravelEntitySpecialRenderer.java
+++ src/main/java/com/mrteroblaze/travelanchorfix/client/render/TravelEntitySpecialRenderer.java
@@ -1,13 +1,14 @@
 package com.mrteroblaze.travelanchorfix.client.render;
 
-import com.enderio.core.client.render.RenderUtil;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import crazypants.enderio.EnderIO;
-import crazypants.enderio.teleport.TravelController;
+import crazypants.enderio.teleport.anchor.BlockTravelAnchor;
 import crazypants.enderio.teleport.anchor.TileTravelAnchor;
+import crazypants.enderio.teleport.TravelController;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.renderer.OpenGlHelper;
-import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
@@ -15,95 +16,92 @@ import net.minecraft.util.IIcon;
 import net.minecraft.util.ResourceLocation;
 import org.lwjgl.opengl.GL11;
 
+@SideOnly(Side.CLIENT)
 public class TravelEntitySpecialRenderer extends TileEntitySpecialRenderer {
 
-    private static final Minecraft mc = Minecraft.getMinecraft();
-    private static final ResourceLocation TEX = new ResourceLocation("enderio", "textures/blocks/TravelAnchor.png");
+    private static IIcon getSelectedOverlayIcon() {
+        try {
+            java.lang.reflect.Field f = BlockTravelAnchor.class.getDeclaredField("selectedOverlayIcon");
+            f.setAccessible(true);
+            return (IIcon) f.get(EnderIO.blockTravelPlatform);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            return null;
+        }
+    }
+
+    private static IIcon getHighlightOverlayIcon() {
+        try {
+            java.lang.reflect.Field f = BlockTravelAnchor.class.getDeclaredField("highlightOverlayIcon");
+            f.setAccessible(true);
+            return (IIcon) f.get(EnderIO.blockTravelPlatform);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            return null;
+        }
+    }
+
+    private final Minecraft mc = Minecraft.getMinecraft();
+    private final RenderManager rm = RenderManager.instance;
+    private final TravelController tc = TravelController.instance;
 
     @Override
     public void renderTileEntityAt(TileEntity te, double x, double y, double z, float partialTick) {
         if (!(te instanceof TileTravelAnchor)) {
             return;
         }
+
         TileTravelAnchor anchor = (TileTravelAnchor) te;
-
-        TravelController tc = TravelController.instance;
-
-        String label = anchor.getLabel();
-        if (label == null || label.isEmpty()) {
-            return;
-        }
-
-        if (!tc.canSeeBlock(mc.thePlayer, anchor.xCoord, anchor.yCoord, anchor.zCoord)) {
-            return;
-        }
-
-        double distSq = mc.thePlayer.getDistanceSq(anchor.xCoord + 0.5, anchor.yCoord + 0.5, anchor.zCoord + 0.5);
-        if (distSq > TravelController.instance.getMaxDistanceSq()) {
-            return;
-        }
-
-        renderLabel(anchor, x, y, z, label);
-    }
-
-    private void renderLabel(TileTravelAnchor anchor, double x, double y, double z, String text) {
         EntityPlayer player = mc.thePlayer;
 
-        FontRenderer fr = mc.fontRenderer;
-        float scale = 0.016666668F * 1.6F;
+        if (player == null || !tc.isTravelAnchor(anchor)) {
+            return;
+        }
+
+        if (!tc.canBlockBeAccessed(mc.theWorld, anchor.xCoord, anchor.yCoord, anchor.zCoord, player)) {
+            return;
+        }
+
+        String name = anchor.getLabel();
+        if (name == null || name.trim().isEmpty()) {
+            return;
+        }
 
         GL11.glPushMatrix();
-        GL11.glTranslated(x + 0.5, y + 1.5, z + 0.5);
+        GL11.glTranslated(x + 0.5, y + 1.2, z + 0.5);
         GL11.glNormal3f(0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(-mc.getRenderManager().playerViewY, 0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(mc.getRenderManager().playerViewX, 1.0F, 0.0F, 0.0F);
-        GL11.glScalef(-scale, -scale, scale);
+
+        GL11.glRotatef(-rm.playerViewY, 0.0F, 1.0F, 0.0F);
+        GL11.glRotatef(rm.playerViewX, 1.0F, 0.0F, 0.0F);
+
+        GL11.glScalef(-0.016F * 1.6F, -0.016F * 1.6F, 0.016F * 1.6F);
 
         GL11.glDisable(GL11.GL_LIGHTING);
         GL11.glDepthMask(false);
         GL11.glDisable(GL11.GL_DEPTH_TEST);
         GL11.glEnable(GL11.GL_BLEND);
-        OpenGlHelper.glBlendFunc(770, 771, 1, 0);
 
-        Tessellator tess = Tessellator.instance;
-        int textW = fr.getStringWidth(text);
-        int halfW = textW / 2;
+        FontRenderer fr = mc.fontRenderer;
+        int textWidth = fr.getStringWidth(name) / 2;
 
-        // фон под текст
+        // фон
         GL11.glDisable(GL11.GL_TEXTURE_2D);
-        tess.startDrawingQuads();
-        tess.setColorRGBA_F(0, 0, 0, 0.25F);
-        tess.addVertex(-halfW - 2, -2, 0.0D);
-        tess.addVertex(-halfW - 2, 9, 0.0D);
-        tess.addVertex(halfW + 2, 9, 0.0D);
-        tess.addVertex(halfW + 2, -2, 0.0D);
-        tess.draw();
+        GL11.glBegin(GL11.GL_QUADS);
+        GL11.glColor4f(0.0F, 0.0F, 0.0F, 0.25F);
+        GL11.glVertex3f(-textWidth - 2, -2, 0.0F);
+        GL11.glVertex3f(-textWidth - 2, 9, 0.0F);
+        GL11.glVertex3f(textWidth + 2, 9, 0.0F);
+        GL11.glVertex3f(textWidth + 2, -2, 0.0F);
+        GL11.glEnd();
         GL11.glEnable(GL11.GL_TEXTURE_2D);
 
-        // сам текст
-        fr.drawString(text, -halfW, 0, 0xFFFFFF);
+        // текст
+        fr.drawStringWithShadow(name, -textWidth, 0, 0xFFFFFF);
 
         GL11.glEnable(GL11.GL_DEPTH_TEST);
         GL11.glDepthMask(true);
         GL11.glEnable(GL11.GL_LIGHTING);
-        GL11.glDisable(GL11.GL_BLEND);
 
         GL11.glPopMatrix();
-    }
-
-    private IIcon getOverlayIcon(boolean selected) {
-        try {
-            if (selected) {
-                java.lang.reflect.Field f = EnderIO.blockTravelPlatform.getClass().getDeclaredField("selectedOverlayIcon");
-                f.setAccessible(true);
-                return (IIcon) f.get(EnderIO.blockTravelPlatform);
-            } else {
-                java.lang.reflect.Field f = EnderIO.blockTravelPlatform.getClass().getDeclaredField("highlightOverlayIcon");
-                f.setAccessible(true);
-                return (IIcon) f.get(EnderIO.blockTravelPlatform);
-            }
-        } catch (Exception e) {
-            return null;
-        }
     }
 }
