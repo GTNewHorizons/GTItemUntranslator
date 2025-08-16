d720b7c347070b4a7f0b9da5919ade6eda5445e9
mrTeroblaze <mramaterus@gmail.com>
2025-08-16 11:49:31 +0300

    test build 0.0.160-test

--- src/main/java/com/mrteroblaze/travelanchorfix/client/render/TravelEntitySpecialRenderer.java
+++ src/main/java/com/mrteroblaze/travelanchorfix/client/render/TravelEntitySpecialRenderer.java
@@ -1,27 +1,24 @@
 package com.mrteroblaze.travelanchorfix.client.render;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.FontRenderer;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.IItemRenderer.ItemRenderType;
-
-import org.lwjgl.opengl.GL11;
-
+import com.enderio.core.client.render.RenderUtil;
 import crazypants.enderio.EnderIO;
 import crazypants.enderio.teleport.TravelController;
 import crazypants.enderio.teleport.anchor.TileTravelAnchor;
-import crazypants.enderio.api.teleport.TravelSource;
-import com.enderio.core.client.render.RenderUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 
 public class TravelEntitySpecialRenderer extends TileEntitySpecialRenderer {
 
     private static final Minecraft mc = Minecraft.getMinecraft();
+    private static final ResourceLocation TEX = new ResourceLocation("enderio", "textures/blocks/TravelAnchor.png");
 
     @Override
     public void renderTileEntityAt(TileEntity te, double x, double y, double z, float partialTick) {
@@ -31,9 +28,6 @@ public class TravelEntitySpecialRenderer extends TileEntitySpecialRenderer {
         TileTravelAnchor anchor = (TileTravelAnchor) te;
 
         TravelController tc = TravelController.instance;
-        if (!tc.isTravelAnchor(anchor)) {
-            return;
-        }
 
         String label = anchor.getLabel();
         if (label == null || label.isEmpty()) {
@@ -49,57 +43,67 @@ public class TravelEntitySpecialRenderer extends TileEntitySpecialRenderer {
             return;
         }
 
-        // === рисуем текст над якорем ===
+        renderLabel(anchor, x, y, z, label);
+    }
+
+    private void renderLabel(TileTravelAnchor anchor, double x, double y, double z, String text) {
+        EntityPlayer player = mc.thePlayer;
+
+        FontRenderer fr = mc.fontRenderer;
+        float scale = 0.016666668F * 1.6F;
+
         GL11.glPushMatrix();
         GL11.glTranslated(x + 0.5, y + 1.5, z + 0.5);
         GL11.glNormal3f(0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(-RenderManager.instance.playerViewY, 0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(RenderManager.instance.playerViewX, 1.0F, 0.0F, 0.0F);
-        float scale = 0.016666668F * 1.6F;
+        GL11.glRotatef(-mc.getRenderManager().playerViewY, 0.0F, 1.0F, 0.0F);
+        GL11.glRotatef(mc.getRenderManager().playerViewX, 1.0F, 0.0F, 0.0F);
         GL11.glScalef(-scale, -scale, scale);
 
         GL11.glDisable(GL11.GL_LIGHTING);
         GL11.glDepthMask(false);
         GL11.glDisable(GL11.GL_DEPTH_TEST);
         GL11.glEnable(GL11.GL_BLEND);
+        OpenGlHelper.glBlendFunc(770, 771, 1, 0);
 
-        FontRenderer fr = mc.fontRenderer;
-        int textW = fr.getStringWidth(label) / 2;
-
-        // фон
         Tessellator tess = Tessellator.instance;
+        int textW = fr.getStringWidth(text);
+        int halfW = textW / 2;
+
+        // фон под текст
         GL11.glDisable(GL11.GL_TEXTURE_2D);
         tess.startDrawingQuads();
-        tess.setColorRGBA_F(0.0F, 0.0F, 0.0F, 0.25F);
-        tess.addVertex(-textW - 1, -1, 0.0D);
-        tess.addVertex(-textW - 1, 8, 0.0D);
-        tess.addVertex(textW + 1, 8, 0.0D);
-        tess.addVertex(textW + 1, -1, 0.0D);
+        tess.setColorRGBA_F(0, 0, 0, 0.25F);
+        tess.addVertex(-halfW - 2, -2, 0.0D);
+        tess.addVertex(-halfW - 2, 9, 0.0D);
+        tess.addVertex(halfW + 2, 9, 0.0D);
+        tess.addVertex(halfW + 2, -2, 0.0D);
         tess.draw();
         GL11.glEnable(GL11.GL_TEXTURE_2D);
 
         // сам текст
-        fr.drawString(label, -textW, 0, 553648127);
+        fr.drawString(text, -halfW, 0, 0xFFFFFF);
+
         GL11.glEnable(GL11.GL_DEPTH_TEST);
         GL11.glDepthMask(true);
-        fr.drawString(label, -textW, 0, -1);
-
         GL11.glEnable(GL11.GL_LIGHTING);
         GL11.glDisable(GL11.GL_BLEND);
-        GL11.glColor4f(1, 1, 1, 1);
+
         GL11.glPopMatrix();
     }
 
-    // ==================
-    // вспомогательный метод (как в оригинале)
-    private void renderEntity(RenderManager rm, EntityItem ei, ItemStack is, boolean isBlock, float scale) {
-        ei.getEntityItem().stackSize = 1;
-        GL11.glPushMatrix();
-        if (isBlock) {
-            GL11.glTranslatef(0.0F, 0.4F, 0.0F);
-            GL11.glRotatef(90.0F, 1.0F, 0.0F, 0.0F);
+    private IIcon getOverlayIcon(boolean selected) {
+        try {
+            if (selected) {
+                java.lang.reflect.Field f = EnderIO.blockTravelPlatform.getClass().getDeclaredField("selectedOverlayIcon");
+                f.setAccessible(true);
+                return (IIcon) f.get(EnderIO.blockTravelPlatform);
+            } else {
+                java.lang.reflect.Field f = EnderIO.blockTravelPlatform.getClass().getDeclaredField("highlightOverlayIcon");
+                f.setAccessible(true);
+                return (IIcon) f.get(EnderIO.blockTravelPlatform);
+            }
+        } catch (Exception e) {
+            return null;
         }
-        rm.renderEntityWithPosYaw(ei, 0.0D, 0.0D, 0.0D, 0.0F, 0.0F);
-        GL11.glPopMatrix();
     }
 }
