05d61197afcbd3b1bb90e28be3c4006888ea787c
mrTeroblaze <mramaterus@gmail.com>
2025-08-16 05:33:12 +0300

    test build 0.0.98-test

--- src/main/java/com/mrteroblaze/travelanchorfix/client/render/TravelEntitySpecialRenderer.java
+++ src/main/java/com/mrteroblaze/travelanchorfix/client/render/TravelEntitySpecialRenderer.java
@@ -1,108 +1,113 @@
 package com.mrteroblaze.travelanchorfix.client.render;
 
-import com.gtnewhorizons.angelica.client.font.BatchingFontRenderer;
 import com.enderio.core.client.render.RenderUtil;
+import com.gtnewhorizons.angelica.client.font.BatchingFontRenderer;
 import crazypants.enderio.EnderIO;
 import crazypants.enderio.teleport.anchor.TileTravelAnchor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
-import net.minecraft.util.ResourceLocation;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL12;
-
-import java.lang.reflect.Field;
 
 public class TravelEntitySpecialRenderer extends TileEntitySpecialRenderer {
 
-    private static final Logger LOG = LogManager.getLogger("TravelAnchorFix");
-
-    private final FontRenderer fr = Minecraft.getMinecraft().fontRenderer;
     private final BatchingFontRenderer bfr;
 
-    public TravelEntitySpecialRenderer(BatchingFontRenderer batched) {
-        this.bfr = batched;
+    public TravelEntitySpecialRenderer() {
+        BatchingFontRenderer tmp = null;
+        try {
+            tmp = new BatchingFontRenderer(Minecraft.getMinecraft().fontRenderer);
+            System.out.println("[TravelAnchorFix] BatchingFontRenderer инициализирован");
+        } catch (Throwable t) {
+            System.out.println("[TravelAnchorFix] BatchingFontRenderer недоступен, fallback на FontRenderer");
+        }
+        this.bfr = tmp;
     }
 
     @Override
-    public void renderTileEntityAt(TileEntity te, double x, double y, double z, float partialTicks) {
-        LOG.info("[TravelAnchorFix] Rendering anchor at {}, {}, {}", x, y, z);
-
+    public void renderTileEntityAt(TileEntity te, double x, double y, double z, float partialTick) {
         if (!(te instanceof TileTravelAnchor)) {
             return;
         }
+
         TileTravelAnchor anchor = (TileTravelAnchor) te;
 
-        String text = anchor.getLabel();
-        LOG.info("[TravelAnchorFix] Anchor label: '{}'", text);
-
-        if (text == null || text.trim().isEmpty()) {
-            return;
+        // Подсветка рамкой (как раньше)
+        IIcon overlay = getSelectedOverlayIcon();
+        if (overlay != null) {
+            RenderUtil.renderQuad2D(x, y, z, 1, 1, overlay);
         }
+
+        // Рендер текста над якорем
+        String toRender = anchor.getLabel();
+        if (toRender != null && !toRender.isEmpty()) {
+            System.out.println("[TravelAnchorFix] Рендер текста: \"" + toRender + "\" у якоря на " + x + "," + y + "," + z);
+            renderFloatingText(toRender, (float) x, (float) y, (float) z);
+        }
+    }
+
+    private void renderFloatingText(String text, float x, float y, float z) {
+        FontRenderer fr = this.func_147498_b();
+        if (fr == null) return;
 
         GL11.glPushMatrix();
-        GL11.glTranslated(x + 0.5, y + 1.5, z + 0.5);
+        GL11.glTranslatef(x + 0.5F, y + 1.5F, z + 0.5F);
+
+        // Поворот к игроку
         GL11.glNormal3f(0.0F, 1.0F, 0.0F);
-        GL11.glRotatef(-this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
-        GL11.glScalef(-0.025F, -0.025F, 0.025F);
+        GL11.glRotatef(-Minecraft.getMinecraft().getRenderManager().playerViewY, 0.0F, 1.0F, 0.0F);
+        GL11.glRotatef(Minecraft.getMinecraft().getRenderManager().playerViewX, 1.0F, 0.0F, 0.0F);
 
-        Tessellator tess = Tessellator.instance;
+        float scale = 0.016666668F * 1.6F;
+        GL11.glScalef(-scale, -scale, scale);
 
-        int textW = (bfr != null ? bfr.getStringWidth(text) : fr.getStringWidth(text));
-        int baseX = -textW / 2;
-
+        Tessellator tessellator = Tessellator.instance;
         GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glDepthMask(false);
         GL11.glDisable(GL11.GL_DEPTH_TEST);
         GL11.glEnable(GL11.GL_BLEND);
+        OpenGlHelper.glBlendFunc(770, 771, 1, 0);
 
-        // рамка фона
-        tess.startDrawingQuads();
-        tess.setColorRGBA_F(0, 0, 0, 0.25f);
-        tess.addVertex(baseX - 2, -2, 0);
-        tess.addVertex(baseX - 2, 9, 0);
-        tess.addVertex(baseX + textW + 2, 9, 0);
-        tess.addVertex(baseX + textW + 2, -2, 0);
-        tess.draw();
+        int width = (bfr != null ? bfr.getStringWidth(text) : fr.getStringWidth(text)) / 2;
+        System.out.println("[TravelAnchorFix] Ширина текста: " + width);
 
-        // текст
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        tessellator.startDrawingQuads();
+        tessellator.setColorRGBA_F(0.0F, 0.0F, 0.0F, 0.25F);
+        tessellator.addVertex(-width - 1, -1, 0.0D);
+        tessellator.addVertex(-width - 1, 8, 0.0D);
+        tessellator.addVertex(width + 1, 8, 0.0D);
+        tessellator.addVertex(width + 1, -1, 0.0D);
+        tessellator.draw();
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+
         if (bfr != null) {
-            LOG.info("[TravelAnchorFix] Using BatchFontRenderer");
-            bfr.drawString(text, baseX, 0, 0xFFFFFF, true);
+            System.out.println("[TravelAnchorFix] Использую BatchingFontRenderer для текста");
+            bfr.drawString(text, -width, 0, 0x80FFFFFF);
         } else {
-            LOG.info("[TravelAnchorFix] Using default FontRenderer");
-            fr.drawString(text, baseX, 0, 0xFFFFFF);
+            System.out.println("[TravelAnchorFix] Использую стандартный FontRenderer для текста");
+            fr.drawString(text, -width, 0, 553648127);
+            GL11.glEnable(GL11.GL_DEPTH_TEST);
+            GL11.glDepthMask(true);
+            fr.drawString(text, -width, 0, -1);
         }
 
-        GL11.glEnable(GL11.GL_DEPTH_TEST);
         GL11.glEnable(GL11.GL_LIGHTING);
         GL11.glDisable(GL11.GL_BLEND);
-
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         GL11.glPopMatrix();
     }
 
     private IIcon getSelectedOverlayIcon() {
         try {
-            Field f = EnderIO.blockTravelPlatform.getClass().getDeclaredField("selectedOverlayIcon");
+            java.lang.reflect.Field f = EnderIO.blockTravelPlatform.getClass().getDeclaredField("selectedOverlayIcon");
             f.setAccessible(true);
             return (IIcon) f.get(EnderIO.blockTravelPlatform);
         } catch (Exception e) {
-            LOG.error("Failed to access selectedOverlayIcon", e);
-            return null;
-        }
-    }
-
-    private IIcon getHighlightOverlayIcon() {
-        try {
-            Field f = EnderIO.blockTravelPlatform.getClass().getDeclaredField("highlightOverlayIcon");
-            f.setAccessible(true);
-            return (IIcon) f.get(EnderIO.blockTravelPlatform);
-        } catch (Exception e) {
-            LOG.error("Failed to access highlightOverlayIcon", e);
             return null;
         }
     }
